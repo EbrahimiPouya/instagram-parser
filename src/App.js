a112ac8b59e47fe5543bc5a4c414f77ae351e746
pouya <p.ebrahimi163@gmail.com>
2020-11-21 10:35:08 +0330

    minimize again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class App extends Component {
 
     componentDidMount() {
-
+        fetch('https://www.instagram.com/p/CH0P-qYhgxZ/?__a=1')
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
     }
 
     render() {
