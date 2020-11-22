aa3c6f327aebc38a0f2b9a02ec204fcf10dab9c6
pouya <p.ebrahimi163@gmail.com>
2020-11-22 10:24:50 +0330

    add default parameters to parser function

--- src/InstagramParser.js
+++ src/InstagramParser.js
@@ -1,4 +1,6 @@
-export default function (postId , callback){
+export default function (postId='' , callback=(data)=>{
+    console.log(data)
+}){
     let url = `https://www.instagram.com/p/${postId}/?__a=1`
     fetch(url)
         .then(response => response.json())
